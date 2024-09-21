3269c947a5faab1e8e86179cef72ae62a7de5f69
Risa Yamamoto <yamari2422@gmail.com>
2024-09-20 19:37:44 -0700

    Finished exercises

--- tests/convertTemp.test.js
+++ tests/convertTemp.test.js
@@ -3,6 +3,16 @@
 // Test case 2: Converting 104 deg fahrenheit will return 40 deg celsius
 // Both test cases need to pass
 
-const Converter = require('../src/convertTemp')
+const Converter = require("../src/convertTemp");
 
-const convert = new Converter()
+const convert = new Converter();
+
+// Test case 1
+test("Converting 30 deg celsius will return 86 deg fahrenheit", () => {
+  expect(convert.convertToFahrenheit(30)).toBe(86);
+});
+
+// Test case 2
+test("Converting 104 deg fahrenheit will return 40 deg celsius", () => {
+  expect(convert.convertToCelsius(104)).toBe(40);
+});
--- tests/counter.test.js
+++ tests/counter.test.js
@@ -4,4 +4,14 @@
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
-const counter = require('../src/counter')
+const counter = require("../src/counter");
+
+// Test case 1
+test("counter(3, 'increment') will eventually return 3", () => {
+  return expect(counter(3, "increment")).resolves.toBe(3);
+});
+
+// Test case 2
+test("counter(3, 'decrement') will eventually return 0", () => {
+  return expect(counter(3, "decrement")).resolves.toBe(0);
+});
